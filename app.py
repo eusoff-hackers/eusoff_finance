7ce4838fcc3e3ee63217d94facdf86661c47c6d0
chokxy <xinyan12345@gmail.com>
2021-01-21 11:39:29 +0800

    added formA1

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ import datetime
 app = Flask(__name__)
 app.debug = True
 
+# everything is stored in this dictionary
 dict = {}
 
 @app.route('/')
 def basic_info():
     return render_template('basic_info.html')
 
-# either determine Code Number in this function OR give prompts on what is the correct code number
-# Method 1: use the count from basic_info.html to determine num of claim forms to fill in per type in claims.html
-# Method 2: can give 10 inputs where they can leave blank for empty claims then maybe html side we put in default text
-# LEARN how to pull data from template to function
+# incomplete: Auto-generate Ref number (eg: SMC2021-number > [COMM][AY]-[NUM][T/C/E])
 @app.route('/claims', methods = ['POST', 'GET'])
 def claims():
     # print(request.form)
@@ -20,11 +18,10 @@ def claims():
     if request.method == 'POST':
         return render_template('claims.html')
 
-# Insert basic and claim form info into form.html
+# incomplete: Auto-generate Code number
 # IF GOT TIME: Make this a downloadable form through button click, on this html list the things they need to prepare for WQ
-# LEARN how to present data from function to template
-@app.route('/form', methods = ['POST', 'GET'])
-def form():
+@app.route('/formB', methods = ['POST', 'GET'])
+def formB():
     dict.update(request.form)
     name = dict["name"]
     matric = dict["matric"]
@@ -35,12 +32,17 @@ def form():
     datetimeobject = datetime.datetime.strptime(old_date, '%Y-%m-%d')
     date = datetimeobject.strftime('%d/%m/%Y')
 
-    print(dict)
+    # print(dict)
     if request.method == 'POST':
         return render_template('form.html', name = name, matric = matric, contact = contact, event = event, ref = ref, date = date)
 
-# IN PROGRESS: Upload docs (https://www.tutorialspoint.com/flask/flask_file_uploading.htm)
-# IN PROGRESS: Mail straight to WQ before printing (https://www.tutorialspoint.com/flask/flask_mail.htm)
+# incomplete!
+@app.route('/formA1')
+def formA1():
+    return render_template('formA1.html')
+
+# IN PROGRESS: Upload receipts (https://www.tutorialspoint.com/flask/flask_file_uploading.htm)
+# IN PROGRESS: Mail? For those that need approval first (https://www.tutorialspoint.com/flask/flask_mail.htm)
 # Deployment (https://www.tutorialspoint.com/flask/flask_deployment.htm)
 
 if __name__ == '__main__':
